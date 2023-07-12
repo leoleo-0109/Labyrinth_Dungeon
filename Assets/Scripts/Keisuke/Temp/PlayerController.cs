9866e042903aad82ac37aae18856fb6e45d4243d
LAPTOP-R9GPHUCK\keisu <keisuke20151114@gmail.com>
2023-07-12 17:00:17 +0900

    MainGameシーンのインスタンス設定更新

--- Assets/Scripts/Keisuke/Temp/PlayerController.cs
+++ Assets/Scripts/Keisuke/Temp/PlayerController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,31 +11,36 @@ public class PlayerController : MonoBehaviour
     public static bool eventFlag = false;
     private bool buttonPressed = false;
     private bool buttonPressedRequest = false;
-    void Update()
+    private CompositeDisposable disposable = new CompositeDisposable();
+    void Start()
     {
         Move();
-        ButtonSSSSSS();
+        WarpButton();
     }
     public void Move()
     {
-        Vector3 pos = transform.position;
-        if(Input.GetKey(KeyCode.W))
+        this.UpdateAsObservable()
+        .Subscribe(_ =>
         {
-            pos.z += dfjklsfkljsfkljs * Time.deltaTime;
-        }
-                if(Input.GetKey(KeyCode.A))
-        {
-            pos.x -= dfjklsfkljsfkljs * Time.deltaTime;
-        }
-                if(Input.GetKey(KeyCode.S))
-        {
-            pos.z -= dfjklsfkljsfkljs * Time.deltaTime;
-        }
-                if(Input.GetKey(KeyCode.D))
-        {
-            pos.x += dfjklsfkljsfkljs * Time.deltaTime;
-        }
-        transform.position = pos;
+            Vector3 pos = transform.position;
+            if(Input.GetKey(KeyCode.W))
+            {
+                pos.z += dfjklsfkljsfkljs * Time.deltaTime;
+            }
+                    if(Input.GetKey(KeyCode.A))
+            {
+                pos.x -= dfjklsfkljsfkljs * Time.deltaTime;
+            }
+                    if(Input.GetKey(KeyCode.S))
+            {
+                pos.z -= dfjklsfkljsfkljs * Time.deltaTime;
+            }
+                    if(Input.GetKey(KeyCode.D))
+            {
+                pos.x += dfjklsfkljsfkljs * Time.deltaTime;
+            }
+            transform.position = pos;
+        }).AddTo(disposable);
     }
     private void OnTriggerStay(Collider other)
     {
@@ -44,24 +52,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void ButtonSSSSSS(){
-                                if(buttonPressedRequest)
+    public void WarpButton()
+    {
+        this.UpdateAsObservable()
+        .Subscribe(_ =>
+        {
+            if(buttonPressedRequest)
+            {
+                Debug.Log("ボタン入力準備完了");
+                if(Input.GetKey(KeyCode.K) && !buttonPressed)
                 {
-                    Debug.Log("ボタン入力準備完了");
-                    if(Input.GetKey(KeyCode.K) && !buttonPressed)
-                    {
-                        Debug.Log("2F");
-                        eventFlag = true;
-                        buttonPressed = true;
-                        Debug.Log(eventFlag);
-                    }
-                    else if(Input.GetKey(KeyCode.K) && buttonPressed)
-                    {
-                        Debug.Log("現在ボタン入力を受け付けません");
-                        buttonPressed = false;
-                        buttonPressedRequest = false;
-                    }
+                    Debug.Log("2F");
+                    eventFlag = true;
+                    buttonPressed = true;
+                    Debug.Log(eventFlag);
                 }
-
+                else if(!Input.GetKey(KeyCode.K) && buttonPressed)
+                {
+                    Debug.Log("現在ボタン入力を受け付けません");
+                    buttonPressed = false;
+                    buttonPressedRequest = false;
+                }
+            }
+        }).AddTo(disposable);
     }
 }
