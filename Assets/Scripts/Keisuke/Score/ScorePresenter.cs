1578facba7030f6857d8e162ce1136a5dc5beea7
LAPTOP-R9GPHUCK\keisu <keisuke20151114@gmail.com>
2023-07-13 12:13:02 +0900

    コンボ実装(仮)まだ動かないと思う

--- Assets/Scripts/Keisuke/Score/ScorePresenter.cs
+++ Assets/Scripts/Keisuke/Score/ScorePresenter.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
+using UniRx;
+using UniRx.Triggers;
 
 public class ScorePresenter : MonoBehaviour
 {
