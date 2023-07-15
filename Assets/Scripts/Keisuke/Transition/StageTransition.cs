8047f5b1aa1eb603b9b7299bec487a04151fe170
chocomint <keisuke20151114@gmail.com>
2023-07-15 14:24:20 +0900

    ワープする度にイベントを発行する処理の追加

--- Assets/Scripts/Keisuke/Transition/StageTransition.cs
+++ Assets/Scripts/Keisuke/Transition/StageTransition.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using Button;
 using BananaClient;
+using UniRx;
+using System;
 
-public class StageTransition : MonoBehaviour
+namespace BananaClient
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject stage;
-    private bool eventTriggered = false;
-    private void OnTriggerStay(Collider other)
+    public class StageTransition : MonoBehaviour
     {
-        if(other.gameObject.CompareTag(TagName.Player))
+        private Subject<Unit> onWarpEventTrigger = new Subject<Unit>();
+        public IObservable<Unit> OnWarpEventTrigger => onWarpEventTrigger;
+        [SerializeField] private GameObject player;
+        [SerializeField] private GameObject stage;
+        private bool eventTriggered = false;
+        private void OnTriggerStay(Collider other)
         {
-            if (EventFlagHolder.eventFlag && !eventTriggered)
+            if(other.gameObject.CompareTag(TagName.Player))
             {
-                Vector3 pos = new Vector3(0,1.6f,0);
-                Vector3 stagePosition = stage.transform.position;
-                stagePosition.y += pos.y;
-                player.transform.position = stagePosition;
-                eventTriggered = true;
-                EventFlagHolder.eventFlag = false;
-                Debug.Log(eventTriggered);
+                if (EventFlagHolder.eventFlag && !eventTriggered)
+                {
+                    onWarpEventTrigger.OnNext(Unit.Default);
+                    Vector3 pos = new Vector3(0,1.6f,0);
+                    Vector3 stagePosition = stage.transform.position;
+                    stagePosition.y += pos.y;
+                    player.transform.position = stagePosition;
+                    eventTriggered = true;
+                    EventFlagHolder.eventFlag = false;
+                    Debug.Log(eventTriggered);
+                }
             }
         }
     }
