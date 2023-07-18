c45a35e42051984591d50d89dd8778e18d70ca8a
chocomint <keisuke20151114@gmail.com>
2023-07-19 08:19:44 +0900

    コメント追加

--- Assets/Scripts/Keisuke/Score/ScoreModel.cs
+++ Assets/Scripts/Keisuke/Score/ScoreModel.cs
@@ -10,12 +10,12 @@ namespace BananaClient
     public class ScoreModel : MonoBehaviour
     {
         private Subject<Unit> onEventTrigger = new Subject<Unit>();
-        public IObservable<Unit> OnEventTrigger => onEventTrigger;
+        public IObservable<Unit> OnEventTrigger => onEventTrigger; // onEventTriggerで値を発行しOnEventTriggerでその値を受け取りSubscribeでOnEventTriggerの値を購読する、今回はUnitなので値なし
         void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.CompareTag(TagName.Player))
             {
-                onEventTrigger.OnNext(Unit.Default);
+                onEventTrigger.OnNext(Unit.Default); // イベント発行
                 gameObject.SetActive(false);
             }
         }
