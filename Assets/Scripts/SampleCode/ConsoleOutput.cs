56ed851f2b235e7dc9c2d09b8838072d6affeec6
DESKTOP-3KRKU4U\Owner <leo-20000109@outlook.jp>
2023-07-23 23:03:56 +0900

    TitleSceneの追加 GameSceneUIの追加 Scene移動用のスクリプトを追加

--- Assets/Scripts/SampleCode/ConsoleOutput.cs
+++ Assets/Scripts/SampleCode/ConsoleOutput.cs
@@ -2,25 +2,23 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Button
+
+public class ConsoleOutput : MonoBehaviour
 {
-    public class ConsoleOutput : MonoBehaviour
+    public ESPSerialHandler espSerialHandler;
+    public NanoSerialHandler nanoSerialHandler;
+
+    void Start()
     {
-        public ESPSerialHandler espSerialHandler;
-        public NanoSerialHandler nanoSerialHandler;
+        espSerialHandler.OnDataReceived += OnDataReceived;
+        nanoSerialHandler.OnDataReceived += OnDataReceived;
+    }
 
-        void Start()
-        {
-            espSerialHandler.OnDataReceived += OnDataReceived;
-            nanoSerialHandler.OnDataReceived += OnDataReceived;
-        }
-
-        void OnDataReceived(string message)
-        {
-            var data = message.Split(
-                new string[] { "\n" }, System.StringSplitOptions.None);
-            if (data.Length != 1) return;
-            Debug.Log(data[0]);
-        }
-    }   
+    void OnDataReceived(string message)
+    {
+        var data = message.Split(
+            new string[] { "\n" }, System.StringSplitOptions.None);
+        if (data.Length != 1) return;
+        Debug.Log(data[0]);
+    }
 }
--- Assets/Scripts/SampleCode/NanoSerialHandler.cs
+++ Assets/Scripts/SampleCode/NanoSerialHandler.cs
@@ -3,93 +3,79 @@ using System.Collections;
 using System.IO.Ports;
 using System.Threading;
 
-namespace Button
+
+public class NanoSerialHandler : MonoBehaviour
 {
-    public class NanoSerialHandler : MonoBehaviour
+    public delegate void SerialDataReceivedEventHandler(string message);
+    public event SerialDataReceivedEventHandler OnDataReceived;
+
+    [SerializeField, Header("nano:COM6")] private string portName = "COM6";
+    //public string portName = "COM6";
+    public int baudRate = 115200;
+
+    private SerialPort serialPort_;
+    private Thread thread_;
+    private bool isRunning_ = false;
+
+    private string message_;
+    private bool isNewMessageReceived_ = false;
+
+    void Awake()
     {
-        public delegate void SerialDataReceivedEventHandler(string message);
-        public event SerialDataReceivedEventHandler OnDataReceived;
+        Debug.Log(portName);
+        Open();
+    }
 
-        [SerializeField, Header("nano:COM6")] private string portName = "COM6";
-        //public string portName = "COM6";
-        public int baudRate = 115200;
-
-        private SerialPort serialPort_;
-        private Thread thread_;
-        private bool isRunning_ = false;
-
-        private string message_;
-        private bool isNewMessageReceived_ = false;
-
-        void Awake()
+    void Update()
+    {
+        if (isNewMessageReceived_)
         {
-            Debug.Log(portName);
-            Open();
+            OnDataReceived(message_);
+        }
+    }
+
+    void OnDestroy()
+    {
+        Close();
+    }
+
+    private void Open()
+    {
+        serialPort_ = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+        serialPort_.Open();
+
+        isRunning_ = true;
+
+        thread_ = new Thread(Read);
+        thread_.Start();
+    }
+
+    private void Close()
+    {
+        isRunning_ = false;
+
+        if (thread_ != null && thread_.IsAlive)
+        {
+            thread_.Join();
         }
 
-        void Update()
+        if (serialPort_ != null && serialPort_.IsOpen)
         {
-            if (isNewMessageReceived_)
-            {
-                OnDataReceived(message_);
-            }
+            serialPort_.Close();
+            serialPort_.Dispose();
         }
+    }
 
-        void OnDestroy()
-        {
-            Close();
-        }
-
-        private void Open()
-        {
-            serialPort_ = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
-            serialPort_.Open();
-
-            isRunning_ = true;
-
-            thread_ = new Thread(Read);
-            thread_.Start();
-        }
-
-        private void Close()
-        {
-            isRunning_ = false;
-
-            if (thread_ != null && thread_.IsAlive)
-            {
-                thread_.Join();
-            }
-
-            if (serialPort_ != null && serialPort_.IsOpen)
-            {
-                serialPort_.Close();
-                serialPort_.Dispose();
-            }
-        }
-
-        private void Read()
-        {
-            while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
-            {
-                try
-                {
-                    // if (serialPort_.BytesToRead > 0) {
-                    message_ = serialPort_.ReadLine();
-                    isNewMessageReceived_ = true;
-                    // }
-                }
-                catch (System.Exception e)
-                {
-                    Debug.LogWarning(e.Message);
-                }
-            }
-        }
-
-        public void Write(string message)
+    private void Read()
+    {
+        while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
         {
             try
             {
-                serialPort_.Write(message);
+                // if (serialPort_.BytesToRead > 0) {
+                message_ = serialPort_.ReadLine();
+                isNewMessageReceived_ = true;
+                // }
             }
             catch (System.Exception e)
             {
@@ -97,5 +83,18 @@ namespace Button
             }
         }
     }
+
+    public void Write(string message)
+    {
+        try
+        {
+            serialPort_.Write(message);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
 }
+
 
