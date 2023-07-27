64f077b8bf6506de7786606b21a413c2f0dd8c22
LAPTOP-R9GPHUCK\keisu <keisuke20151114@gmail.com>
2023-07-27 13:10:48 +0900

    add: タイムアイテム取得時実装

---
+++ Assets/Scripts/Keisuke/Time/TimerModel.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UniRx;
+
+namespace BananaClient
+{
+    public class TimerModel : MonoBehaviour
+    {
+        private Subject<Unit> timerItem = new Subject<Unit>();
+        public IObservable<Unit> TimerItemObserver => timerItem;
+        private void OnCollisionEnter(Collision other)
+        {
+            if(other.gameObject.CompareTag(TagName.Player)){
+                timerItem.OnNext(Unit.Default);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
