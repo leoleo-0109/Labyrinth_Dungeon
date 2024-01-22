93fae648ecf0a20fd20152c92b530a17042cb6f1
taya91820 <sab769737@gmail.com>
2024-01-22 16:13:51 +0900

    update firetrap param

--- Assets/Scripts/Fire/ScriptableObject/FireTrapData.cs
+++ Assets/Scripts/Fire/ScriptableObject/FireTrapData.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Datas/FireGimmickData")]
-public class FireTrapData : ScriptableObject 
+public class FireTrapData : ScriptableObject
 {
     [field: SerializeField,Header("")] public float rotationSpeed { get; private set; }
     [field: SerializeField,Header("")] public float rotationDirection { get; private set; }
     [field: SerializeField,Header("")] public float stopAngle { get; private set; }
+    [field: SerializeField,Header("")] public float particleDisplayTime { get; private set;} // パーティクルの表示時間
+    [field: SerializeField,Header("")] public float particleHideTime { get; private set; } // パーティクルの非表示時間
+    [field: SerializeField,Header("")] public float particleToggleTime { get; private set; } // パーティクルの表示/非表示を切り替える時間
 }
