282b93f36e06c4e05746b20df834fa73cefe0118
DESKTOP-3KRKU4U\Owner <leo-20000109@outlook.jp>
2024-01-27 15:05:06 +0900

    フォント、文字の差し替え、コントローラー用に各Sceneを追加

--- Assets/Scripts/Leo/SceneController.cs
+++ Assets/Scripts/Leo/SceneController.cs
@@ -3,41 +3,172 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    // KeyBoard—p
     // TitleScene
-    public void OnClickStartButton()
+    public void OnClickStartButton_KeyBoard()
     {
-        SceneManager.LoadScene("IntroductionScene");
+        SceneManager.LoadScene("ModeSelectScene_KeyBoard");
     }
 
     // TitleScene
-    public void OnClickRankingButton()
+    public void OnClickRankingButton_KeyBoard()
     {
-        SceneManager.LoadScene("RankingScene");
+        SceneManager.LoadScene("RankingScene_KeyBoard");
     }
 
     // IntroductionScene
-    public void OnClickGameKeyButton()
+    public void OnClickStoryModeGameButton_KeyBoard()
     {
-        SceneManager.LoadScene("KeyBoardMainGame");
+        SceneManager.LoadScene("StoryModeGameScene_KeyBoard");
     }
-    public void OnClickGameNanoButton()
+
+    // ModeSelectScene
+    public void OnClickStageSelectButton_KeyBoard()
     {
-        SceneManager.LoadScene("MainGameNanoVer");
+        SceneManager.LoadScene("StageSelectScene_KeyBoard");
     }
-     public void OnClickGameESPButton()
+
+    // StageSelectScene
+    public void OnClickStage1Button_KeyBoard()
     {
-        SceneManager.LoadScene("MainGameESP32Ver");
+        SceneManager.LoadScene("Stage1GameScene_KeyBoard");
+    }
+
+    // StageSelectScene
+    public void OnClickStage2Button_KeyBoard()
+    {
+        SceneManager.LoadScene("Stage2GameScene_KeyBoard");
+    }
+
+    // StageSelectScene
+    public void OnClickStage3Button_KeyBoard()
+    {
+        SceneManager.LoadScene("Stage3GameScene_KeyBoard");
+    }
+
+    // StageSelectScene
+    public void OnClickReturnModeSelectButton_KeyBoard()
+    {
+        SceneManager.LoadScene("ModeSelectScene_KeyBoard");
     }
 
     // RankingScene
-    public void OnClickTitleButton()
+    public void OnClickReturnTitleButton_KeyBoard()
     {
-        SceneManager.LoadScene("Title");
+        SceneManager.LoadScene("TitleScene_KeyBoard");
+    }
+
+    ///////////////////////////////////////////////////////////
+
+    // Nano—p
+    // TitleScene
+    public void OnClickStartButton_Nano()
+    {
+        SceneManager.LoadScene("ModeSelectScene_Nano");
     }
 
     // TitleScene
-    public void OnClickRetryButton()
+    public void OnClickRankingButton_Nano()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("RankingScene_Nano");
+    }
+
+    // IntroductionScene
+    public void OnClickStoryModeGameButton_Nano()
+    {
+        SceneManager.LoadScene("StoryModeGameScene_Nano");
+    }
+
+    // ModeSelectScene
+    public void OnClickStageSelectButton_Nano()
+    {
+        SceneManager.LoadScene("StageSelectScene_Nano");
+    }
+
+    // StageSelectScene
+    public void OnClickStage1Button_Nano()
+    {
+        SceneManager.LoadScene("Stage1GameScene_Nano");
+    }
+
+    // StageSelectScene
+    public void OnClickStage2Button_Nano()
+    {
+        SceneManager.LoadScene("Stage2GameScene_Nano");
+    }
+
+    // StageSelectScene
+    public void OnClickStage3Button_Nano()
+    {
+        SceneManager.LoadScene("Stage3GameScene_Nano");
+    }
+
+    // StageSelectScene
+    public void OnClickReturnModeSelectButton_Nano()
+    {
+        SceneManager.LoadScene("ModeSelectScene_Nano");
+    }
+
+    // RankingScene
+    public void OnClickReturnTitleButton_Nano()
+    {
+        SceneManager.LoadScene("TitleScene_Nano");
+    }
+
+    ///////////////////////////////////////////////////////////
+
+    // ESP32—p
+    // TitleScene
+    public void OnClickStartButton_ESP32()
+    {
+        SceneManager.LoadScene("ModeSelectScene_ESP32");
+    }
+
+    // TitleScene
+    public void OnClickRankingButton_ESP32()
+    {
+        SceneManager.LoadScene("RankingScene_ESP32");
+    }
+
+    // IntroductionScene
+    public void OnClickStoryModeGameButton_ESP32()
+    {
+        SceneManager.LoadScene("StoryModeGameScene_ESP32");
+    }
+
+    // ModeSelectScene
+    public void OnClickStageSelectButton_ESP32()
+    {
+        SceneManager.LoadScene("StageSelectScene_ESP32");
+    }
+
+    // StageSelectScene
+    public void OnClickStage1Button_ESP32()
+    {
+        SceneManager.LoadScene("Stage1GameScene_ESP32");
+    }
+
+    // StageSelectScene
+    public void OnClickStage2Button_ESP32()
+    {
+        SceneManager.LoadScene("Stage2GameScene_ESP32");
+    }
+
+    // StageSelectScene
+    public void OnClickStage3Button_ESP32()
+    {
+        SceneManager.LoadScene("Stage3GameScene_ESP32");
+    }
+
+    // StageSelectScene
+    public void OnClickReturnModeSelectButton_ESP32()
+    {
+        SceneManager.LoadScene("ModeSelectScene_ESP32");
+    }
+
+    // RankingScene
+    public void OnClickReturnTitleButton_ESP32()
+    {
+        SceneManager.LoadScene("TitleScene_ESP32");
     }
 }
