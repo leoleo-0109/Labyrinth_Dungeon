dc279b70af0b03b12f5bd53ddb6f198c032e272a
taya91820 <sab769737@gmail.com>
2024-02-07 23:29:53 +0900

    fix keep now time

--- Assets/Scripts/UI/Score/EndGameScoreLoader.cs
+++ Assets/Scripts/UI/Score/EndGameScoreLoader.cs
@@ -14,17 +14,25 @@ public class EndGameScoreLoader : MonoBehaviour
     {
         gameState.GameClearObserver.Subscribe(_ =>
         {
-            int currentTime = timerPresenter.keepNowTime * 100; // タイムを100倍にする
+            timerPresenter.OnKeepTime();
+            Debug.Log("namati" + timerPresenter.keepNowTime);
+            int currentTime = timerPresenter.keepNowTime * 10; // タイムを100倍にする
+            Debug.Log("currentTime" + currentTime);
             int currentScore = Mathf.RoundToInt(scorePresenter.score);  // スコアを四捨五入して整数に変換
             int totalScore = currentScore + currentTime; // スコアとタイムを合計してトータルスコアを作成
+            Debug.Log("totalScore" + totalScore);
             ScoreManager.Instance.Score = totalScore;
         }).AddTo(disposables);
 
         gameState.GameOverObserver.Subscribe(_ =>
         {
-            int currentTime = timerPresenter.keepNowTime * 100; // タイムを100倍にする
+            timerPresenter.OnKeepTime();
+            Debug.Log("namati" + timerPresenter.keepNowTime);
+            int currentTime = timerPresenter.keepNowTime * 10; // タイムを100倍にする
+            Debug.Log("currentTime" + currentTime);
             int currentScore = Mathf.RoundToInt(scorePresenter.score);  // スコアを四捨五入して整数に変換
             int totalScore = currentScore + currentTime; // スコアとタイムを合計してトータルスコアを作成
+            Debug.Log("totalScore" + totalScore);
             ScoreManager.Instance.Score = totalScore;
         }).AddTo(disposables);
     }
