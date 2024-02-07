7abb20615082555458d3f8dbca5a6a8ef8be28ef
komyoyasuyuki <yasukomyo0811@gmail.com>
2024-02-07 18:17:17 +0900

    CSV読み込みの正常化

--- Assets/Scripts/Komyo/Ranking/RankingTextManager.cs
+++ Assets/Scripts/Komyo/Ranking/RankingTextManager.cs
@@ -18,24 +18,18 @@ public class RankingTextManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI thirdScore;
     [SerializeField] private TextMeshProUGUI fourthScore;
     [SerializeField] private TextMeshProUGUI fifthScore;
-    public string first;
-    public string second;
-    public string third;
-    public string fourth;
-    public string fifth;
-    
     public void SetText()
     {
-        fifthName.text = sortDate.ReturnRanking(1).name;
-        fifthScore.text = sortDate.ReturnRanking(1).score.ToString();
-        fourthName.text = sortDate.ReturnRanking(2).name;
-        fourthScore.text = sortDate.ReturnRanking(2).score.ToString();
+        firstName.text = sortDate.ReturnRanking(1).name;
+        firstScore.text = sortDate.ReturnRanking(1).score.ToString();
+        secondName.text = sortDate.ReturnRanking(2).name;
+        secondScore.text = sortDate.ReturnRanking(2).score.ToString();
         thirdName.text = sortDate.ReturnRanking(3).name;
         thirdScore.text = sortDate.ReturnRanking(3).score.ToString();
-        secondName.text = sortDate.ReturnRanking(4).name;
-        secondScore.text = sortDate.ReturnRanking(4).score.ToString();
-        firstName.text = sortDate.ReturnRanking(5).name;
-        firstScore.text = sortDate.ReturnRanking(5).score.ToString();
+        fourthName.text = sortDate.ReturnRanking(4).name;
+        fourthScore.text = sortDate.ReturnRanking(4).score.ToString();
+        fifthName.text = sortDate.ReturnRanking(5).name;
+        fifthScore.text = sortDate.ReturnRanking(5).score.ToString();
         saveToCSV.Save(firstName.text + "," + firstScore.text, secondName.text + "," + secondScore.text, thirdName.text + "," + thirdScore.text, fourthName.text + "," + fourthScore.text, fifthName.text + "," + fifthScore.text);
     }
 }
